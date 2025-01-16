f1c2af761619df4b441ec6debb7b94454725160d
Shravani-Dake <shravanidake17@gmail.com>
2025-01-16 11:06:45 +0530

    Update Multiplication_table.cpp

--- Multiplication_table.cpp
+++ Multiplication_table.cpp
@@ -4,11 +4,9 @@ using namespace std;
 int main() {
     int num;
 
-    // Ask the user to enter a number
     cout << "Enter a number to display its multiplication table: ";
     cin >> num;
-
-    // Generate and display the multiplication table
+    
     cout << "Multiplication Table of " << num << ":\n";
     for (int i = 1; i <= 10; i++) {
         cout << num << " x " << i << " = " << num * i << endl;
