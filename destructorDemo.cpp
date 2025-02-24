6e6cf6a84a80cd39d88d0485e3709a779f97e5be
Shravani-Dake <shravanidake17@gmail.com>
2025-02-24 01:49:10 -0800

    Add files via upload

---
+++ destructorDemo.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+	private:
+		int *p;
+		
+	public:
+		
+		void getInfo()
+		{
+			cout<<*p<<endl;
+		}
+		
+		void setInfo(int v)
+		{
+			*this->p=v;
+		}
+		
+		Demo(int x)
+		{
+			p=new int(x);
+		}
+		
+		Demo(const Demo &obj)
+		{
+			p=new int(*(obj.p));
+		}
+		~Demo()
+		{
+			delete p;
+			cout<<"Destrustor is Called "<<endl;
+		}
+};
+int main()
+{
+	
+	Demo d1(111);
+	d1.getInfo();
+	
+	Demo d2(d1);
+	d2.getInfo();
+	d2.setInfo(222);
+	
+	
+	d2.getInfo();
+	d1.getInfo();
+	d2.~Demo(); 
+	d1.getInfo();
+	d2.getInfo();
+	
+	return 0;
+	
+}
